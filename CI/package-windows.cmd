4aafebd1aae4e94e9b35f187be86c4f00f83bae0
EddyGharbi <eddygharbiema@gmail.com>
2020-10-05 17:10:58 -0700

    Build and distribute from Azure

---
+++ CI/package-windows.cmd
@@ -0,0 +1,4 @@
+mkdir soundtrack-plugin-64
+
+cp build/soundtrack/RelWithDebInfo/soundtrack-plugin-64.dll soundtrack-plugin-64/soundtrack-plugin-64.dll
+cp build/soundtrack/RelWithDebInfo/soundtrack-plugin-64.pdb soundtrack-plugin-64/soundtrack-plugin-64.pdb
